8118b38b18f75cecf87f8b0946e52f61f8115b9e
Mikael Olsson <mikael.olsson@emmio.se>
2021-02-10 14:23:01 +0100

    Updated code samples

--- code/08 - streams/streams/JavaStreams.java
+++ code/08 - streams/streams/JavaStreams.java
@@ -52,6 +52,72 @@ public class JavaStreams {
 			.map(String::toLowerCase)
 			.filter(x -> x.startsWith("a"))
 			.forEach(System.out::println);
+			
+		// 8. Stream rows from text file, sort, filter, and print
+		String filename="/Users/micke/www/projects/ec education/java/avancerad java/ec-avancerad_java/code/08 - streams/streams/bands.txt";
+	
+		Stream<String> bands = Files.lines(Paths.get(filename));
+		bands
+			.sorted()
+			.filter(x -> x.length() > 13)
+			.forEach(System.out::println);
+		bands.close();
+		
+		// 9. Stream rows from text file and save to List
+		filename="/Users/micke/www/projects/ec education/java/avancerad java/ec-avancerad_java/code/08 - streams/streams/bands.txt";
+	
+		List<String> bands2 = Files.lines(Paths.get(filename))
+			.filter(x -> x.contains("jit"))
+			.collect(Collectors.toList());
+		bands2.forEach(x -> System.out.println(x));
+		
+		// 10. Stream rows from CSV file and count
+		filename="/Users/micke/www/projects/ec education/java/avancerad java/ec-avancerad_java/code/08 - streams/streams/data.txt";
+	
+		Stream<String> rows1 = Files.lines(Paths.get(filename));
+		int rowCount = (int)rows1
+			.map(x -> x.split(","))
+            .filter(x -> x.length == 3)
+			.count();
+		System.out.println(rowCount + " rows.");
+		rows1.close();
+		
+		// 11. Stream rows from CSV file, parse data from rows
+		filename="/Users/micke/www/projects/ec education/java/avancerad java/ec-avancerad_java/code/08 - streams/streams/data.txt";
 
+		Stream<String> rows2 = Files.lines(Paths.get(filename));
+		rows2
+			.map(x -> x.split(","))
+            .filter(x -> x.length == 3)
+			.filter(x -> Integer.parseInt(x[1]) > 15)
+			.forEach(x -> System.out.println(x[0] + "  " + x[1] + "  " + x[2]));
+		rows2.close();
+		
+		// 12. Stream rows from CSV file, store fields in HashMap
+		filename="/Users/micke/www/projects/ec education/java/avancerad java/ec-avancerad_java/code/08 - streams/streams/data.txt";
+
+		Stream<String> rows3 = Files.lines(Paths.get(filename));
+		Map<String, Integer> map = new HashMap<>();
+		map = rows3
+			.map(x -> x.split(","))
+            .filter(x -> x.length == 3)
+			.filter(x -> Integer.parseInt(x[1]) > 15)
+			.collect(Collectors.toMap(
+                x -> x[0],
+                x -> Integer.parseInt(x[1])));
+		rows3.close();
+		for (String key : map.keySet()) {
+			System.out.println(key + "  " + map.get(key));
+		}
+			
+		// 13. Reduction - sum
+		double total = Stream.of(7.3, 1.5, 4.8)
+			.reduce(0.0, (Double a, Double b) -> a + b);
+		System.out.println("Total = " + total);
+		
+		// 14. Reduction - summary statistics
+		IntSummaryStatistics summary = IntStream.of(7, 2, 19, 88, 73, 4, 10)
+			.summaryStatistics();
+		System.out.println(summary);
 	}
 }
