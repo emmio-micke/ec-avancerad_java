f30343ce8182b8d02890485e4f67e6caa4e0b0d3
Mikael Olsson <mikael.olsson@emmio.se>
2021-02-10 12:09:41 +0100

    WIP: code samples

--- code/08 - streams/streams/JavaStreams.java
+++ code/08 - streams/streams/JavaStreams.java
@@ -1,8 +1,6 @@
 package streams;
 
 import java.lang.String;
-import java.util.Arrays;
-import java.util.List;
 import java.util.stream.*;
 import java.util.*;
 import java.nio.file.*;
@@ -10,5 +8,50 @@ import java.io.IOException;
 
 public class JavaStreams {
 	public static void main(String[] args) throws IOException {
+		// 1. Integer Stream
+		IntStream
+			.range(1, 11)
+			.forEach(System.out::println);
+		
+        // 2. Integer Stream, skip 5
+		IntStream
+			.range(1, 10)
+			.skip(5)  // Skips the first 5
+			.forEach(x -> System.out.println(x));
+		
+		// 3. Integer Stream with sum
+		System.out.println(
+		IntStream
+			.range(1, 5)
+			.sum());
+		System.out.println();
+			
+		// 4. Stream.of, sorted and findFirst
+		Stream.of("Ava", "Aneri", "Alberto")
+			.sorted()
+			.findFirst()
+			.ifPresent(System.out::println);
+			
+		// 5. Stream from Array, sort, filter and print
+		String[] names = {"Al", "Ankit", "Kushal", "Brent", "Sarika", "amanda", "Hans", "Shivika", "Sarah"};
+		Arrays.stream(names)	// same as Stream.of(names)
+			.filter(x -> x.startsWith("S"))
+			.sorted()
+			.forEach(System.out::println);
+					
+		// 6. average of squares of an int array
+		Arrays.stream(new int[] {2, 4, 6, 8, 10})
+			.map(x -> x * x)
+			.average()
+			.ifPresent(System.out::println);
+		
+		// 7. Stream from List, filter and print
+		List<String> people = Arrays.asList("Al", "Ankit", "Brent", "Sarika", "amanda", "Hans", "Shivika", "Sarah");
+		people
+			.stream()
+			.map(String::toLowerCase)
+			.filter(x -> x.startsWith("a"))
+			.forEach(System.out::println);
+
 	}
 }
